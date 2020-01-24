d003f4b2e90052a43f7db1e6870b7cda8915d96d
Matt <matthew.morley.ca@gmail.com>
2020-01-24 08:54:30 -0800

    More gradle memes (still broken)

--- src/dev/java/edu/wpi/first/wpilibj/wpilibStateSpace/DevMain.java
+++ src/dev/java/edu/wpi/first/wpilibj/wpilibStateSpace/DevMain.java
@@ -9,6 +9,7 @@ package edu.wpi.first.wpilibj.wpilibStateSpace;
 
 import edu.wpi.first.hal.HALUtil;
 import edu.wpi.first.networktables.NetworkTablesJNI;
+import edu.wpi.first.wpilibj.math.DrakeJNI;
 import edu.wpi.first.wpiutil.RuntimeDetector;
 
 public final class DevMain {
@@ -17,9 +18,11 @@ public final class DevMain {
    */
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    System.out.println(RuntimeDetector.getPlatformPath());
-    System.out.println(NetworkTablesJNI.now());
-    System.out.println(HALUtil.getHALRuntimeType());
+    try {
+      DrakeJNI.forceLoad();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   private DevMain() {
